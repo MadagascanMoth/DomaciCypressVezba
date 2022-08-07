da4de8cce936c8a6207e5badebc211699692d3ce
MadagascanMoth <madagascanmoth@gmail.com>
2022-08-07 20:04:34 +0200

    lokatori

--- cypress/e2e/LoginPageLocators.cy.js
+++ cypress/e2e/LoginPageLocators.cy.js
@@ -20,7 +20,7 @@ describe("Login test cases", ()=>{
     })
 
     it("Login without required email", ()=>{
-        cy.get(locators.login.emailInput).clear();
+        cy.get(locators.login.emailInput);
         cy.get(locators.login.passwordInput).type("357753357");
         cy.get(locators.login.submitButton).click();
 
@@ -28,29 +28,29 @@ describe("Login test cases", ()=>{
 
     it("Login without required password", ()=>{
         cy.get(locators.login.emailInput).type("red@gmail.com");
-        cy.get(locators.login.passwordInput).clear();
+        cy.get(locators.login.passwordInput);
         cy.get(locators.login.submitButton).click();
 
     })
 
     it("Login with invalid email", ()=>{
-        cy.get(locators.login.emailInput).clear().type("redgmail.com");
+        cy.get(locators.login.emailInput).type("redgmail.com");
         cy.get(locators.login.passwordInput).type("357753357");
         cy.get(locators.login.submitButton).click();
 
     })
 
     it("Login with invalid password", ()=>{
-        cy.get(locators.login.emailInput).clear().type("red@gmail.com");
-        cy.get(locators.login.passwordInput).clear().type("aaaaaa");
+        cy.get(locators.login.emailInput).type("red@gmail.com");
+        cy.get(locators.login.passwordInput).type("aaaaaa");
         cy.get(locators.login.submitButton).click();
 
     })
 
 
     it("Login with valid credentials",()=>{
-        cy.get(locators.login.emailInput).clear().type("red@gmail.com");
-        cy.get(locators.login.passwordInput).clear().type("357753357");
+        cy.get(locators.login.emailInput).type("red@gmail.com");
+        cy.get(locators.login.passwordInput).type("357753357");
         cy.get(locators.login.submitButton).click();
         cy.get(locators.header.logoutButton).click();
     })
--- cypress/e2e/RegistrationPageLocators.cy.js
+++ cypress/e2e/RegistrationPageLocators.cy.js
@@ -26,121 +26,121 @@ describe ("Registration test cases",()=>{
 
     })
     it("Registration without valid First Name-less than 2 characters",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("H");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("H");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid First Name-more than 255 characters",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("lRGKLlWlZDilHd6LBnDf8XfV0cm4ajOjGUeaaxUpDgS6FJUGniYw6I0C1sigMRik2pfpvJdVn7xuCtTlFornvg8hcO2Nj8Law14en0xP8qHQrec9H9KlbFDKv6QzXtGHCotVrVr2Q4qWkwh4GCSyVNFpCnHnsjDN1WLX6JYrn9ygIFlmMBf2FSDn0DjnEas3obBqMrrWjwQprLuUCxejkjNX0y5VJpBIkeu8mDAQFRksCL2jnjAsbOQvjPQlUbD");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("lRGKLlWlZDilHd6LBnDf8XfV0cm4ajOjGUeaaxUpDgS6FJUGniYw6I0C1sigMRik2pfpvJdVn7xuCtTlFornvg8hcO2Nj8Law14en0xP8qHQrec9H9KlbFDKv6QzXtGHCotVrVr2Q4qWkwh4GCSyVNFpCnHnsjDN1WLX6JYrn9ygIFlmMBf2FSDn0DjnEas3obBqMrrWjwQprLuUCxejkjNX0y5VJpBIkeu8mDAQFRksCL2jnjAsbOQvjPQlUbD");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid Last Name-less than 2 characters",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("H");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("H");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid Last Name -more than 255 characters",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("lRGKLlWlZDilHd6LBnDf8XfV0cm4ajOjGUeaaxUpDgS6FJUGniYw6I0C1sigMRik2pfpvJdVn7xuCtTlFornvg8hcO2Nj8Law14en0xP8qHQrec9H9KlbFDKv6QzXtGHCotVrVr2Q4qWkwh4GCSyVNFpCnHnsjDN1WLX6JYrn9ygIFlmMBf2FSDn0DjnEas3obBqMrrWjwQprLuUCxejkjNX0y5VJpBIkeu8mDAQFRksCL2jnjAsbOQvjPQlUbD");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("lRGKLlWlZDilHd6LBnDf8XfV0cm4ajOjGUeaaxUpDgS6FJUGniYw6I0C1sigMRik2pfpvJdVn7xuCtTlFornvg8hcO2Nj8Law14en0xP8qHQrec9H9KlbFDKv6QzXtGHCotVrVr2Q4qWkwh4GCSyVNFpCnHnsjDN1WLX6JYrn9ygIFlmMBf2FSDn0DjnEas3obBqMrrWjwQprLuUCxejkjNX0y5VJpBIkeu8mDAQFRksCL2jnjAsbOQvjPQlUbD");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid email-without @",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarijagmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarijagmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid email-without .",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmailcom");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmailcom");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid email-with space",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija @gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija @gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid password - less than 8 characters",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("hava");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("hava");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("hava");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("hava");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration without valid password - without at least one digit",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration with wrong confirmed password",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
     })
 
     it("Registration with already taken email",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("red@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("red@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
     })
@@ -150,11 +150,11 @@ describe ("Registration test cases",()=>{
 //Positive test case
 
     it("Registration with required requirements",()=>{
-        cy.get(locators.register.inputFirstName).clear().type("Havarija");
-        cy.get(locators.register.inputLastName).clear().type("Havarija");
-        cy.get(locators.register.inputRegisterEmail).clear().type("havarija@gmail.com");
-        cy.get(locators.register.inputRegisterPassword).clear().type("havarija1");
-        cy.get(locators.register.inputRegisterConfirmationPassword).clear().type("havarija1");
+        cy.get(locators.register.inputFirstName).type("Havarija");
+        cy.get(locators.register.inputLastName).type("Havarija");
+        cy.get(locators.register.inputRegisterEmail).type("havarija@gmail.com");
+        cy.get(locators.register.inputRegisterPassword).type("havarija1");
+        cy.get(locators.register.inputRegisterConfirmationPassword).type("havarija1");
         cy.get(locators.register.registerCheckBox).check();
         cy.get(locators.register.registerSubmitButton).click();
 
